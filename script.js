7249bc59af9caaca51098a2aeac28c7fc1bc7bc5
Pokdee <loselife61@gmail.com>
2023-03-18 09:39:57 +0700

    Implement btn moving function

--- script.js
+++ script.js
@@ -2,23 +2,44 @@
 const passIn = document.querySelector(".pass_in");
 const btn = document.querySelector(".btn-submit");
 
-const num = "abcdabc";
-let pass_len = num.length;
+let pass_len;
 
-const checker = function (num) {};
-btn.addEventListener("mouseover", function () {
-  //   console.log(pass_len);
-  if (pass_len < 8) {
+const checker = function (e) {
+  console.log();
+  pass_len = e.target.value.length;
+};
+
+const btnRunner = function (password) {
+  console.log(this);
+  if (!this) return;
+  if (password < 8) {
     const parentWidth = this.parentElement.offsetWidth;
     const elementWidth = this.offsetWidth;
     const style = window.getComputedStyle(this);
-    console.log(parentWidth, elementWidth);
-    console.log(style.marginRight);
+    const right = parseInt(style.marginRight.slice(0, -2));
+    const left = parseInt(style.marginLeft.slice(0, -2));
 
-    // const value = parentWidth - elementWidth - 100 + "px";
-    // console.log(value);
-    // this.style.marginLeft = value;
+    const value = parentWidth - elementWidth - 100 + "px";
+
+    if (!right || !left) {
+      this.style.marginLeft = value;
+    }
+    if (right > left) {
+      this.style.marginRight = "0px";
+
+      this.style.marginLeft = value;
+    }
+    if (left > right) {
+      this.style.marginLeft = "0px";
+
+      this.style.marginRight = value;
+    }
   }
-});
+};
 
 passIn.addEventListener("input", (e) => checker(e));
+
+btn.addEventListener("mouseover", function () {
+  console.log(this);
+  btnRunner(pass_len);
+});
