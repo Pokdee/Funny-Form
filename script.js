0b3cb3a3a4c8abd1d340f6713980574973d57416
Pokdee <loselife61@gmail.com>
2023-03-19 09:36:58 +0700

    Just some  clean up

--- script.js
+++ script.js
@@ -14,56 +14,30 @@ passIn.addEventListener("input", (e) => checker(e));
 btn.addEventListener("mouseover", function () {
   // console.log(this);
   if (!this) return;
+
+  //Data Collection
+
+  const parentWidth = this.parentElement.offsetWidth;
+  const elementWidth = this.offsetWidth;
+  const style = window.getComputedStyle(this);
+  const right = parseInt(style.marginRight.slice(0, -2));
+  const left = parseInt(style.marginLeft.slice(0, -2));
+
+  const value = parentWidth - elementWidth - 100 + "px";
+
+  //funcs
+
   if (pass_len < 8) {
-    const parentWidth = this.parentElement.offsetWidth;
-    const elementWidth = this.offsetWidth;
-    const style = window.getComputedStyle(this);
-    const right = parseInt(style.marginRight.slice(0, -2));
-    const left = parseInt(style.marginLeft.slice(0, -2));
-
-    const value = parentWidth - elementWidth - 100 + "px";
-
     if (!right || !left) {
       this.style.marginLeft = value;
     }
     if (right > left) {
-      this.style.marginRight = "0px";
-
-      this.style.marginLeft = value;
+      btn.style.marginRight = "0px";
+      btn.style.marginLeft = value;
     }
     if (left > right) {
-      this.style.marginLeft = "0px";
-
-      this.style.marginRight = value;
+      btn.style.marginLeft = "0px";
+      btn.style.marginRight = value;
     }
   }
 });
-
-// //BTn running function
-// const btnRunner = function (password) {
-//   console.log(this);
-//   if (!this) return;
-//   if (password < 8) {
-//     const parentWidth = this.parentElement.offsetWidth;
-//     const elementWidth = this.offsetWidth;
-//     const style = window.getComputedStyle(this);
-//     const right = parseInt(style.marginRight.slice(0, -2));
-//     const left = parseInt(style.marginLeft.slice(0, -2));
-
-//     const value = parentWidth - elementWidth - 100 + "px";
-
-//     if (!right || !left) {
-//       this.style.marginLeft = value;
-//     }
-//     if (right > left) {
-//       this.style.marginRight = "0px";
-
-//       this.style.marginLeft = value;
-//     }
-//     if (left > right) {
-//       this.style.marginLeft = "0px";
-
-//       this.style.marginRight = value;
-//     }
-//   }
-// };
