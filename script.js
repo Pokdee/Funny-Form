71ebe278917c4385a75056c988adc3c985d9c289
Pokdee <loselife61@gmail.com>
2023-03-19 09:00:19 +0700

    Connect btn runner to password checker

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const btn = document.querySelector(".btn-submit");
 
 let pass_len;
 
+//pass checking
 const checker = function (e) {
-  console.log();
   pass_len = e.target.value.length;
 };
 
-const btnRunner = function (password) {
-  console.log(this);
+passIn.addEventListener("input", (e) => checker(e));
+
+btn.addEventListener("mouseover", function () {
+  // console.log(this);
   if (!this) return;
-  if (password < 8) {
+  if (pass_len < 8) {
     const parentWidth = this.parentElement.offsetWidth;
     const elementWidth = this.offsetWidth;
     const style = window.getComputedStyle(this);
@@ -35,11 +37,33 @@ const btnRunner = function (password) {
       this.style.marginRight = value;
     }
   }
-};
-
-passIn.addEventListener("input", (e) => checker(e));
-
-btn.addEventListener("mouseover", function () {
-  console.log(this);
-  btnRunner(pass_len);
 });
+
+// //BTn running function
+// const btnRunner = function (password) {
+//   console.log(this);
+//   if (!this) return;
+//   if (password < 8) {
+//     const parentWidth = this.parentElement.offsetWidth;
+//     const elementWidth = this.offsetWidth;
+//     const style = window.getComputedStyle(this);
+//     const right = parseInt(style.marginRight.slice(0, -2));
+//     const left = parseInt(style.marginLeft.slice(0, -2));
+
+//     const value = parentWidth - elementWidth - 100 + "px";
+
+//     if (!right || !left) {
+//       this.style.marginLeft = value;
+//     }
+//     if (right > left) {
+//       this.style.marginRight = "0px";
+
+//       this.style.marginLeft = value;
+//     }
+//     if (left > right) {
+//       this.style.marginLeft = "0px";
+
+//       this.style.marginRight = value;
+//     }
+//   }
+// };
